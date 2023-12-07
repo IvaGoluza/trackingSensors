34a61e03552782a521e09ab2568196ee9634c959
Iva Goluža <ig53401@fer.hr>
2023-12-08 00:52:23 +0100

    fix

--- KafkaCoordinator/src/main/java/Coordinator.java
+++ KafkaCoordinator/src/main/java/Coordinator.java
@@ -5,40 +5,44 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Coordinator {
 
-    private static void produce() {
+    private static final Logger logger = Logger.getLogger(Coordinator.class.getName());
+
+
+    public static void main(String[] args){
+
         // producer configuration
         Properties producerProperties = new Properties();
         producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        Scanner scanner = new Scanner(System.in);
 
         try (Producer<String, String> producer = new KafkaProducer<>(producerProperties)) {   // try-with-resources for producer closing
 
             while (true) {
-                String TOPIC = "Command";
-                ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, null, "start");
-                producer.send(record);
-                try {
-                    Thread.sleep(120000);  // let the sensors work for 2 minutes
-                } catch (InterruptedException e) {
-                    break;
+                String userInput = scanner.nextLine();
+                if (userInput != null) {
+                    if (userInput.equalsIgnoreCase("start")) {
+                        ProducerRecord<String, String> record = new ProducerRecord<>("Command", null, "start");
+                        producer.send(record);
+                        logger.info("Kafka coordinator starting system sensors.");
+                    } else if (userInput.equalsIgnoreCase("stop")) {
+                        ProducerRecord<String, String> record = new ProducerRecord<>("Command", null, "stop");
+                        producer.send(record);
+                        logger.info("Kafka coordinator stopping system sensors.");
+                        scanner.close();
+                        break;
+                    }
                 }
-                record = new ProducerRecord<>(TOPIC, null, "stop");
-                producer.send(record);
             }
 
         } catch (Exception e) {
             System.out.println("Could not start producer: " + e);
         }
     }
-
-    public static void main(String[] args){
-
-        produce();
-
-    }
-
 }
